4850392ab3c8d470d0b8fe415e0232900eef6d42
shibildas <shibildas@gmail.com>
2023-05-15 14:20:55 +0530

    swagger removed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import { lazy, Suspense } from "react";
 const ExpertRouter = lazy(() => import("./Router/ExpertRouter"));
 const AdminRouter = lazy(() => import("./Router/AdminRouter"));
-const ApiDocs=lazy(()=> import ('./Pages/common/ApiDocs'))
 import { BrowserRouter, Routes, Route} from "./import";
 import ShimmerList from "./Components/Admin/Shimmer/ShimmerList";
 import UserRouter from "./Router/UserRouter";
@@ -25,14 +24,7 @@ function App() {
               </Suspense>
           }
         />
-        <Route
-          exact path="/api-docs"
-          element={
-              <Suspense fallback={<ShimmerList />}>
-                <ApiDocs />
-              </Suspense>
-          }
-        />
+     
       </Routes>
     </BrowserRouter>
   );
