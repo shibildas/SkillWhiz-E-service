ea75878aa5f0584bd750afa16cb41151b5128f93
shibildas <shibildas@gmail.com>
2023-05-15 11:34:09 +0530

    url swagger

--- client/src/Pages/common/ApiDocs.jsx
+++ client/src/Pages/common/ApiDocs.jsx
@@ -3,8 +3,9 @@ import SwaggerUI from 'swagger-ui-react';
 import 'swagger-ui-react/swagger-ui.css';
 
 const ApiDocs = () => {
+    const url=import.meta.env.VITE_SWAGGER
   return (
-    <SwaggerUI url="http://localhost:4000/swagger.json" />
+    <SwaggerUI url={url} />
   );
 };
 
